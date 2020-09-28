f5040f776c1b14c03bb745b649631bd912d7a5bb
shashidar mandalozu <sasitech18@gmail.com>
2020-09-28 08:46:00 +0100

    React Basics: Moving Components Into Separate Files

---
+++ 004.MoveComponentsIntoSeparateFiles/components/MyInfo.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function MyInfo() {
+  return (
+    <div>
+      <h1>Shashi WebDev</h1>
+      <p> A Self - taught Full - Stack Web DevLuvPer who is passionate about Web Development.I taught myself the following skills : </p> 
+      <ul>
+        <li>HTML5</li> 
+        <li>CSS3 </li>
+        <li>BootStrap4</li> 
+        <li>JavaScript</li> 
+        <li>JQuery </li>
+        <li>React</li> 
+        <li>Node</li> 
+        <li>Express</li>
+        <li>SQL</li> 
+        <li>PHP</li> 
+        <li>Python </li>
+        <li>Java</li> 
+        <li>C#</li> 
+        <li>Git & GitHub </li>
+        <li>Command Line Tools</li> 
+      </ul>
+    </div>
+  )
+}
+
+export default MyInfo;
