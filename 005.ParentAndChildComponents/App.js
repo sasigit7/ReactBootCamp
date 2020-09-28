0da2e849784d09ef04f1f790b58e909f9de0d4b8
shashidar mandalozu <sasitech18@gmail.com>
2020-09-28 23:42:44 +0100

    React Basics: Parent And Child Components

---
+++ 005.ParentAndChildComponents/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Navbar from "./components/Navbar";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+
+function App() {
+    return (
+        <div>
+            <Navbar/>
+            <MainContent/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default App;
---
+++ 005.ParentAndChildComponents/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <nav>This is Navigation bar......</nav>
+    )
+}
+
+export default Navbar;
